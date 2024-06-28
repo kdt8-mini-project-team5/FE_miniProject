faaad64dd3cd53b361f7c189a17087a78e6f9a80
BaDook2 <wkdrudqls114@gmail.com>
2024-06-28 18:48:49 +0900

    refactor: mswcomponent fix

--- src/components/MSWComponent.tsx
+++ src/components/MSWComponent.tsx
@@ -8,7 +8,12 @@ function MswComponent({ children }: { children: React.ReactNode }) {
   const isWorkerStarted = useRef(false);
   useEffect(() => {
     async function enableApiMocking() {
-      if (process.env.NODE_ENV === 'development' && !isWorkerStarted.current) {
+      if (process.env.NODE_ENV === 'production') {
+        setIsMocking(true);
+      } else if (
+        process.env.NODE_ENV === 'development' &&
+        !isWorkerStarted.current
+      ) {
         (async () => {
           const { worker } = await import('@/mocks/browser');
           await worker.start();
