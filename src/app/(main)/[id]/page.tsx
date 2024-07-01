0d4af2e063c87a1fc72ec4ee61c0fa77516483ff
BaDook2 <wkdrudqls114@gmail.com>
2024-07-01 13:17:11 +0900

    fix: date

--- src/app/(main)/[id]/page.tsx
+++ src/app/(main)/[id]/page.tsx
@@ -47,9 +47,8 @@ function AccommodationDetail({ params }: { params: { id: string } }) {
   const today = new Date();
   const tomorrow = new Date(today);
   tomorrow.setDate(today.getDate() + 1);
-  const todayToDate = `${today.getFullYear()}-${today.getMonth() < 9 ? '0' : ''}${today.getMonth() + 1}-${today.getDate()}`;
-  const tommorrowToDate = `${tomorrow.getFullYear()}-${tomorrow.getMonth() < 9 ? '0' : ''}${tomorrow.getMonth() + 1}-${tomorrow.getDate()}`;
-
+  const todayToDate = `${today.getFullYear()}-${today.getMonth() < 9 ? '0' : ''}${today.getMonth() + 1}-${today.getDate() < 9 ? '0' : ''}${today.getDate()}`;
+  const tommorrowToDate = `${tomorrow.getFullYear()}-${tomorrow.getMonth() < 9 ? '0' : ''}${tomorrow.getMonth() + 1}-${today.getDate() < 9 ? '0' : ''}${tomorrow.getDate()}`;
   const [data, setData] = useState<IAccommodation | null>(null);
   const [peopleCount, setPeopleCount] = useState<string>('1');
   const [checkInDate, setCheckInDate] = useState<string>(todayToDate);
