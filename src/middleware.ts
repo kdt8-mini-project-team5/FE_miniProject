f61cf092b6f7d05754727ca7b2182b1cbc9b05d4
BaDook2 <wkdrudqls114@gmail.com>
2024-07-04 14:32:31 +0900

    fix: middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,41 +1,41 @@
-// import { NextResponse } from 'next/server';
-// import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-// // 쿠키 이름을 정의합니다.
-// const ACCESS_TOKEN = 'accessToken';
+// 쿠키 이름을 정의합니다.
+const ACCESS_TOKEN = 'accessToken';
 
-// // 미들웨어 함수
-// export function middleware(request: NextRequest) {
-//   const cookie = request.cookies.get(ACCESS_TOKEN);
-//   const protectedPaths = ['/bookingList', '/booking', '/cart'];
+// 미들웨어 함수
+export function middleware(request: NextRequest) {
+  const cookie = request.cookies.get(ACCESS_TOKEN);
+  const protectedPaths = ['/bookingList', '/booking', '/cart'];
 
-//   const authPaths = ['/login', '/signup'];
+  const authPaths = ['/login', '/signup'];
 
-//   const isProtectedPath = protectedPaths.some((path) =>
-//     new URL(request.url).pathname.startsWith(path),
-//   );
-//   const isAuthPath = authPaths.some((path) =>
-//     new URL(request.url).pathname.startsWith(path),
-//   );
-//   if (isProtectedPath) {
-//     if (!cookie) {
-//       return NextResponse.redirect(new URL('/login', request.url));
-//     }
-//   }
-//   if (isAuthPath) {
-//     if (cookie) {
-//       return NextResponse.redirect(new URL('/', request.url));
-//     }
-//   }
-//   return NextResponse.next();
-// }
+  const isProtectedPath = protectedPaths.some((path) =>
+    new URL(request.url).pathname.startsWith(path),
+  );
+  const isAuthPath = authPaths.some((path) =>
+    new URL(request.url).pathname.startsWith(path),
+  );
+  if (isProtectedPath) {
+    if (!cookie) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+  }
+  if (isAuthPath) {
+    if (cookie) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  }
+  return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: [
-//     '/bookingList/:path*',
-//     '/booking/:path*',
-//     '/cart/:path*',
-//     '/login',
-//     '/signup',
-//   ],
-// };
+export const config = {
+  matcher: [
+    '/bookingList/:path*',
+    '/booking/:path*',
+    '/cart/:path*',
+    '/login',
+    '/signup',
+  ],
+};
