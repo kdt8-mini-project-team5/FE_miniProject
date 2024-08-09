a46c8480fa5d20878d4a8a15aeffc55b35d8878d
BaDook2 <wkdrudqls114@gmail.com>
2024-08-09 18:11:11 +0900

    fix: accommodation page date

--- src/components/common/exportDate.ts
+++ src/components/common/exportDate.ts
@@ -1,7 +1,8 @@
 const exportDate = () => {
   const today = new Date();
   const tomorrow = new Date(today);
-  tomorrow.setDate(today.getDate() + 1);
+  tomorrow.setDate(today.getDate() + 2);
+  today.setDate(today.getDate() + 1);
   const todayToDate = `${today.getFullYear()}-${today.getMonth() < 9 ? '0' : ''}${today.getMonth() + 1}-${today.getDate() < 9 ? '0' : ''}${today.getDate()}`;
   const tommorrowToDate = `${tomorrow.getFullYear()}-${tomorrow.getMonth() < 9 ? '0' : ''}${tomorrow.getMonth() + 1}-${today.getDate() < 9 ? '0' : ''}${tomorrow.getDate()}`;
   return { todayToDate, tommorrowToDate };
